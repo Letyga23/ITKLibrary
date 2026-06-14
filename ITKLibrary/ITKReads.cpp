feb452fecb64fe56f8005b5d0c3f526f04068bc9
Всеволод Русакевич <127239601+Letyga23@users.noreply.github.com>
2026-06-14 16:55:04 +0300

    Update ITKReads.cpp

--- ITKLibrary/ITKReads.cpp
+++ ITKLibrary/ITKReads.cpp
@@ -159,96 +159,6 @@ extern "C" __declspec(dllexport) void ITKFileNames(std::string & FolderName, std
 	}
 }
 
-extern "C" __declspec(dllexport) void ITKPatientInfo(std::string & FolderName, std::string & bodyPart, std::string& modality, std::string & patientID)
-{
-	typedef signed short InternalPixelType;
-	const unsigned int Dimension = 3; // The dimension is 3, not 2
-	using InternalImageType = itk::Image<InternalPixelType, Dimension>;
-
-	typedef itk::Image<InternalPixelType, Dimension> ImageType;
-	typedef itk::ImageSeriesReader<ImageType> ReaderType;
-
-	typedef itk::GDCMImageIO ImageIOType;
-	typedef itk::DCMTKSeriesFileNames NamesGeneratorType;
-
-	ImageIOType::Pointer gdcmIO = ImageIOType::New();
-	NamesGeneratorType::Pointer namesGenerator = NamesGeneratorType::New();
-
-	namesGenerator->SetInputDirectory(FolderName);
-
-	const ReaderType::FileNamesContainer& filenames =
-		namesGenerator->GetInputFileNames();
-
-	std::size_t numberOfFileNames = filenames.size();
-	std::cout << numberOfFileNames << std::endl;
-
-
-	ReaderType::Pointer reader = ReaderType::New();
-	reader->SetImageIO(gdcmIO);
-	reader->SetFileNames(filenames);
-
-
-	// Reading Data;
-	try
-	{
-		reader->Update();
-		reader->GetMetaDataDictionary();
-		gdcmIO->GetMetaDataDictionary();// Read header file information;
-
-		//Information assignment
-		char* name = new char[50];
-		char* _patientID = new char[50];
-		char* time = new char[50];
-		char* manufacture = new char[50];
-		char* _modality = new char[50];
-		char* hospital = new char[50];
-		char* sex = new char[50];
-		char* age = new char[50];
-		char* description = new char[100];
-		char* _bodyPart = new char[50];
-
-		ImageIOType::ByteOrder byteOrder;
-		byteOrder = gdcmIO->GetByteOrder();
-
-		unsigned int dim = 0;
-		gdcmIO->GetDimensions(dim);
-		ImageIOType::SizeType imgsize;
-		imgsize = gdcmIO->GetImageSizeInPixels();
-		int componetSize = gdcmIO->GetComponentSize();
-		int dimension = gdcmIO->GetNumberOfDimensions();
-		int ori = 0;
-
-		gdcmIO->GetOrigin(ori);
-		int spa = 0;
-
-		gdcmIO->GetPatientSex(sex);
-		gdcmIO->GetPatientAge(age);
-		gdcmIO->GetStudyDescription(description);
-		gdcmIO->GetSpacing(spa);
-		gdcmIO->GetPatientName(name);
-		gdcmIO->GetModality(_modality);
-		gdcmIO->GetPatientID(_patientID);
-		gdcmIO->GetManufacturer(manufacture);
-		gdcmIO->GetStudyDate(time);
-		gdcmIO->GetBodyPart(_bodyPart);
-		
-		gdcmIO->GetInstitution(hospital);
-		ImageType::SpacingType spacetype;
-		spacetype = reader->GetOutput()->GetSpacing();
-		ImageType::PointType origin;
-		origin = reader->GetOutput()->GetOrigin();
-
-		bodyPart = _bodyPart;
-		modality = _modality;
-		patientID = _patientID;
-	}
-	catch (const itk::ExceptionObject& excp)
-	{
-		std::cout << " Reading Exceptaion Caught" << std::endl;
-		std::cout << excp.what() << std::endl;
-	}
-}
-
 struct HDVolumeInfo
 {
 	int DimX;
@@ -778,8 +688,6 @@ bool ReadDicomSeriesToVolume(const char* firstFilePath, uint8_t** outBuffer, siz
 		return false;
 	}
 }
-
-
 
 
 extern "C" __declspec(dllexport)
